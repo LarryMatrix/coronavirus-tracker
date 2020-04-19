e47f81dd5d1155f2d84ab4081efd45173d12101b
Lawrance Massanja <massanjal4@gmail.com>
2020-04-19 18:58:24 +0300

    Initial commit

---
+++ src/main/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,16 @@
+package io.javabrains.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+		System.out.println("================= CORONA VIRUS APP IS UP AND RUNNING =================");
+	}
+
+}
---
+++ src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package io.javabrains.coronavirustracker.controllers;
+
+import io.javabrains.coronavirustracker.models.LocationStats;
+import io.javabrains.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    final
+    CoronaVirusDataService coronaVirusDataService;
+
+    public HomeController(CoronaVirusDataService coronaVirusDataService) {
+        this.coronaVirusDataService = coronaVirusDataService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
